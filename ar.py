6eacad8105a1d952c9f79cd861168d712ab0a78d
VaishnaviGanagi <vaishnavivaishu8484@gmail.com>
2026-01-03 08:59:09 +0530

    array progran

---
+++ ar.py
@@ -0,0 +1,9 @@
+def main():
+    scores=[33,44,55,66,89]
+    total=sum(scores)
+    average=total/len(scores)
+    print(f"scores:{scores}")
+    print(f"sum:{total}")
+    print(f"average:{average}")
+if __name__=="__main__":
+    main()
